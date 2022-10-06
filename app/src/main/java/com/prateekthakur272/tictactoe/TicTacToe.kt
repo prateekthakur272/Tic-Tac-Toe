49ba9dc2dc5208b841b001e776dcc201b6474354
prateekthakur272 <prateekthakur272@gmail.com>
2022-10-06 15:07:54 +0530

    Tic Tac Toe

--- app/src/main/java/com/prateekthakur272/tictactoe/TicTacToe.kt
+++ app/src/main/java/com/prateekthakur272/tictactoe/TicTacToe.kt
@@ -2,5 +2,8 @@ package com.prateekthakur272.tictactoe
 
 class TicTacToe {
     val boardArray:Array<Byte> = arrayOf(0,0,0,0,0,0,0,0,0)
+    fun isWinner():Boolean{
 
+        return false
+    }
 }
